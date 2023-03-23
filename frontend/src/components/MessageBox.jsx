b039a704b7443af70de0f0738bb21ccbc88eaa45
Caleb <caleblindelien@gmail.com>
2023-03-23 15:58:05 -0500

    Added Loading Icon and error messages

---
+++ frontend/src/components/MessageBox.jsx
@@ -0,0 +1,7 @@
+import Alert from 'react-bootstrap/Alert';
+
+function MessageBox(props) {
+  return <Alert variant={props.variant || 'info'}>{props.children}</Alert>;
+}
+
+export default MessageBox;
